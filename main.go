17a8ae98913f3db2d91690f6c2c7f388106e3bbd
WatchJani <jankokondic84@gmail.com>
2024-10-15 18:29:24 +0200

    init

---
+++ main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+	"path"
+)
+
+func main() {
+	mux := http.NewServeMux()
+
+	fs := http.FileServer(http.Dir("./build"))
+	mux.Handle("/build/", http.StripPrefix("/build/", fs))
+
+	mux.HandleFunc("GET /data/{link}", FileServer)
+
+	http.ListenAndServe(":5000", mux)
+}
+
+func FileServer(w http.ResponseWriter, r *http.Request) {
+	url := r.PathValue("link")
+
+	fullPath := path.Join("./build", url)
+
+	data, err := os.ReadFile(fullPath)
+	if err != nil {
+		http.Error(w, "File not found", http.StatusNotFound)
+		log.Println(err)
+		return
+	}
+	w.Header().Set("Content-Type", http.DetectContentType(data))
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(data)
+}
